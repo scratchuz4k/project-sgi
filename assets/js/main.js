76cbed1a18148ea80a62cc0c6fd24a5af1959d69
Marcos Ferreira <marcos_ferreira@outlook.pt>
2022-12-26 23:27:25 +0000

    animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,29 @@ let misturador = new THREE.AnimationMixer(cena)
 let acoes = []
 let loading = true
 let sizes
+let children = []
 
 let carregador = new THREE.GLTFLoader()
 carregador.load(
     './assets/blender/armario.gltf',
     function (gltf) {
-        model = gltf.scene;
-        console.log(gltf);
-        loading = false
-        model.position.set(0, -1, 0)
-        let bbox = new THREE.Box3().setFromObject(model);
+        gltf.scene.children.forEach((e) => {
+            if (e.name != 'Cube') {
+                e.addEventListener("click", function () { console.log("teste") })
+                children.push(e)
+            }
+        });
+        console.log(children)
+        debugger
+        gltf.scene.position.set(0, -1, 0)
+        let bbox = new THREE.Box3().setFromObject(gltf.scene);
         let helper = new THREE.Box3Helper(bbox, new THREE.Color(0, 255, 0));
+        cena.add(gltf.scene);
+        gltf.animations.forEach((e) => {
+            acoes.push(misturador.clipAction(e));
+        })
         sizes = bbox.getSize(new THREE.Vector3()); // HEREyou get the size
-        cena.add(model);
-        cena.add(helper);
+        // cena.add(helper);
     }
 )
 
@@ -44,10 +53,35 @@ btnCamaraPos.forEach((e) => {
                 break;
 
         }
-        console.log(e.target)
     })
 })
 
+
+
+let btnPlay = document.getElementById("btn_play")
+
+btnPlay.addEventListener('click', function () {
+    acoes[0].play()
+    acoes[1].play()
+    acoes[2].play()
+})
+
+function onPointerMove(event) {
+
+    // calculate pointer position in normalized device coordinates
+    // (-1 to +1) for both components
+
+    rato.x = (event.clientX / window.innerWidth) * 2 - 1;
+    rato.y = - (event.clientY / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(rato, camara);
+
+    const intersects = raycaster.intersectObjects(children)
+    for (let i = 0; i < intersects.length; i++) {
+        console.log(intersects)
+    }
+}
+
+window.addEventListener('mousemove', onPointerMove);
 
 // iniciar animação... 
 animar();
@@ -59,4 +93,7 @@ function animar() {
     renderer.render(cena, camara);
     controlos.update(0.01)
     misturador.update(relogio.getDelta())
+
+    // update the picking ray with the camera and pointer position
+    raycaster.setFromCamera(rato, camara);
 }

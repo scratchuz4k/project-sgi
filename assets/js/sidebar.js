c863ee83a356f1d006cf304a589538f657a16726
Simão Pedro <46787324+Ceffas84@users.noreply.github.com>
2023-01-14 12:58:46 +0000

    wip

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -6,10 +6,23 @@ function toggleSidebar() {
 window.onload = function () {
   let burger = document.querySelectorAll('.burger-btn');
   if (burger.length) {
-      burger.forEach((e) => {
-          e.addEventListener('click', () => {
-              toggleSidebar()
-          })
+    burger.forEach((e) => {
+      e.addEventListener('click', () => {
+        toggleSidebar()
       })
+    })
   }
+}
+
+function renderCategoryList() {
+
+  let list = document.getElementById('categoryList')
+
+  data.categories.forEach((category) => {
+    let item = document.createElement('li')
+    item.id = 'cat' + category.id
+    item.classList.add("list-group-item", "list-group-item-action")
+    item.innerText = category.name
+    list.appendChild(item)
+  })
 }
